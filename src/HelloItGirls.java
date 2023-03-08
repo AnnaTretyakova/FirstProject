980fb7a39f8e0b602f785cbdc43ee39c25a50843
AnnaTretyakova <anntretwork@gmail.com>
2023-03-08 17:55:45 +0100

    second commit

---
+++ src/HelloItGirls.java
@@ -0,0 +1,12 @@
+public class HelloItGirls {
+    public HelloItGirls() {
+    }
+
+    public static void main(String[] args){
+        print();
+    }
+
+    private static void print() {
+        System.out.println("HelloItGirls");
+    }
+}
